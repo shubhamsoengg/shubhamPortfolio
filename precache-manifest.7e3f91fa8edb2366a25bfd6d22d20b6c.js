9c1c790fc7debcb0265ab8dca57031f70a889962
Shubham Goyal <shubh1993goyal@gmail.com>
2020-06-01 19:38:55 -0700

    Updates

---
+++ precache-manifest.7e3f91fa8edb2366a25bfd6d22d20b6c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e229ab49e291be6e285f760d8ae414",
+    "url": "/shubhamPortfolio/index.html"
+  },
+  {
+    "revision": "1d023b60fd5b210461a5",
+    "url": "/shubhamPortfolio/static/css/2.5485aa58.chunk.css"
+  },
+  {
+    "revision": "c72c76920d2f4af25c8d",
+    "url": "/shubhamPortfolio/static/css/main.199a8d5c.chunk.css"
+  },
+  {
+    "revision": "1d023b60fd5b210461a5",
+    "url": "/shubhamPortfolio/static/js/2.5a270ccb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shubhamPortfolio/static/js/2.5a270ccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c72c76920d2f4af25c8d",
+    "url": "/shubhamPortfolio/static/js/main.89085c6f.chunk.js"
+  },
+  {
+    "revision": "cd1da6ba2bfd2c77fc00",
+    "url": "/shubhamPortfolio/static/js/runtime-main.5d3c485d.js"
+  },
+  {
+    "revision": "6b55ca5d85ad426dcd7ac1015beb39f5",
+    "url": "/shubhamPortfolio/static/media/SHUBHAM_GOYAL_RESUME.6b55ca5d.pdf"
+  },
+  {
+    "revision": "090e354901d5fffe8e24a2a18cec419b",
+    "url": "/shubhamPortfolio/static/media/asu_logo.090e3549.png"
+  },
+  {
+    "revision": "d14668c0b36ac07847d2743b974c442a",
+    "url": "/shubhamPortfolio/static/media/aus_open_viz.d14668c0.png"
+  },
+  {
+    "revision": "c88a71ba963bace6b5678d6dad6a991a",
+    "url": "/shubhamPortfolio/static/media/bms_logo.c88a71ba.png"
+  },
+  {
+    "revision": "677ecd488cc6875b7df21c588960b0c6",
+    "url": "/shubhamPortfolio/static/media/covid_viz.677ecd48.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/shubhamPortfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/shubhamPortfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/shubhamPortfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/shubhamPortfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/shubhamPortfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "dbf2df154e4be2e50ebc07cca0b5065c",
+    "url": "/shubhamPortfolio/static/media/ge_logo.dbf2df15.png"
+  },
+  {
+    "revision": "a0ddf3ec7c0cc2452911cabcc0e8e581",
+    "url": "/shubhamPortfolio/static/media/mcf_logo.a0ddf3ec.png"
+  },
+  {
+    "revision": "2829b57c450a25035e9870902fa06cf4",
+    "url": "/shubhamPortfolio/static/media/profile.2829b57c.jpg"
+  },
+  {
+    "revision": "f04808ae8165ae05403a836621c148b1",
+    "url": "/shubhamPortfolio/static/media/profile_2.f04808ae.jpg"
+  },
+  {
+    "revision": "b50b9390818991a438b71c9d7d761a20",
+    "url": "/shubhamPortfolio/static/media/sorter_viz.b50b9390.png"
+  }
+]);
